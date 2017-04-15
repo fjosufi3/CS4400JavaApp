e6d4b37ff1d3a2c7450c2dfd93478e2714f85e93
Chaewon Min <mcw0805@gmail.com>
2017-04-15 02:31:54 -0400

    Resolved conflicts. Added table to pending data points.
    Readded the tablview for pending data points because the changes weren't reflected.

--- JavaApp/src/app/java/controller/Main.java
+++ JavaApp/src/app/java/controller/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main extends Application {
 
     @Override
@@ -14,11 +17,20 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/src/app/java/view/login.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        //Parent root = FXMLLoader.load(getClass().getResource("/app/res/pending_data_points.fxml"));
+
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+//        ConnectionConfiguration con = new ConnectionConfiguration();
+//        con.getConnection();
     }
+
+
 }
