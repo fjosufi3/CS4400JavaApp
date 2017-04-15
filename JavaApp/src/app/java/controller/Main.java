c1ae5a9550e9f1c462264e05b146505b332146b9
Chaewon Min <mcw0805@gmail.com>
2017-04-15 00:39:25 -0400

    Added classes for the some entities, connection. Added TableView for pending data points. Set output folder.
    Added User, POI, UserType (enum), DataType, DataPoint. TableView available for pending data points

--- JavaApp/src/app/java/controller/Main.java
+++ JavaApp/src/app/java/controller/Main.java
@@ -1,16 +1,18 @@
-package sample;
+package app.java.controller;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Pane root = new Pane();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
