282ed893565adbdcf3b061eb8156a56cf377fdb4
Fayaz Josufi <fjosufi3@gatech.edu>
2017-04-15 01:23:20 -0400

    main activity functionality

--- JavaApp/src/app/java/controller/PendingDataController.java
+++ JavaApp/src/app/java/controller/PendingDataController.java
@@ -1,6 +1,8 @@
-package app.java.controller;
+package src.app.java.controller;
 
-import app.java.controller.DataPoint;
+import src.app.java.model.DataPoint;
+import src.app.java.model.POI;
+import src.app.java.model.DataType;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
