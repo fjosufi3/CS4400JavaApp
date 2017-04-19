8e3c5034b56025cd303027dd095319919bbfdabf
Chaewon Min <mcw0805@gmail.com>
2017-04-19 12:31:28 -0400

    Editing POI detail filter feature

--- JavaApp/src/main/app/java/controller/POIDetailController.java
+++ JavaApp/src/main/app/java/controller/POIDetailController.java
@@ -3,6 +3,7 @@ package app.java.controller;
 import app.java.model.ConnectionConfiguration;
 import app.java.model.DataPoint;
 import app.java.model.DataType;
+import app.java.model.FormValidation;
 import com.jfoenix.controls.JFXCheckBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,6 +23,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -43,13 +47,13 @@ public class POIDetailController implements Initializable {
     @FXML
     private JFXCheckBox flagged_checkbox;
     @FXML
-    private TextField minDataVal;
+    private TextField minDataValTextField;
     @FXML
-    private TextField maxDataVal;
+    private TextField maxDataValTextField;
     @FXML
-    private DatePicker startDate_potDeatil;
+    private DatePicker startDate_poiDetail;
     @FXML
-    private DatePicker endDate_potDeatil;
+    private DatePicker endDate_poiDetail;
     @FXML
     private Label typeRequired_label;
     @FXML
@@ -95,7 +99,6 @@ public class POIDetailController implements Initializable {
 
     }
 
-
     @FXML
     private void loadDropDown() {
 
@@ -122,10 +125,102 @@ public class POIDetailController implements Initializable {
     @FXML
     private void onClickApplyFilter() {
 
+        boolean minDataNotEmpty = FormValidation.textFieldNotEmpty(minDataValTextField);
+        boolean maxDataNotEmpty = FormValidation.textFieldNotEmpty(maxDataValTextField);
+        boolean isSelectedType = !(dataTypeBox.getSelectionModel().isEmpty());
+        boolean isFlagged = flagged_checkbox.isSelected();
+        boolean isValidStartDate = startDate_poiDetail.getValue() != null;
+        boolean isValidEndDate = endDate_poiDetail.getValue() != null;
+        boolean isValidRange = isValidStartDate && isValidEndDate && endDate_poiDetail.getValue().compareTo(startDate_poiDetail.getValue()) >= 0;
+
+        String type = null;
+        String minVal = null;
+        String maxVal = null;
+        String flag = null;
+        String dateFlagged = null;
+        String dateStart = null;
+        String dateEnd = null;
+
+        if (isSelectedType) {
+            type = "Type = \'" + dataTypeBox.getSelectionModel()
+                    .getSelectedItem().toString() + "\' ";
+        }
+
+        if (minDataNotEmpty) {
+            minVal = "Data_Value >= " + Integer.parseInt(minDataValTextField.getText());
+        }
+
+        if (maxDataNotEmpty) {
+            maxVal = "Data_Value <= " + Integer.parseInt(maxDataValTextField.getText());
+        }
+
+
+        if (isValidStartDate) {
+            dateStart = "Date_Flagged >= \'" + startDate_poiDetail.getValue().toString() + "\'";
+            //dateStart = dateStart_view_poi.getValue().toString();
+        }
+
+        if (isValidEndDate) {
+            dateEnd = "Date_Flagged <= \'" + endDate_poiDetail.getValue().toString() + "\'";
+            //dateEnd = endDate_poiDetail.getValue().toString();
+        }
+
+        if (isFlagged) {
+            flag = "Flag = TRUE";
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            dateFlagged = dateFormat.format(new Date());
+            System.out.println("date flagged: " + dateFlagged);
+        }
+
+
+        if (type == null && minVal == null && maxVal == null && flag == null
+                && dateStart == null && dateEnd == null) { //checks empty fields
+            Alert errorAlert = new Alert(Alert.AlertType.INFORMATION);
+            errorAlert.setHeaderText("Error!");
+            errorAlert.setContentText("No fields are entered.");
+            errorAlert.showAndWait();
+
+        } else if ((dateStart == null && dateEnd != null)
+                || (dateStart != null && dateEnd == null)) {
+
+            //only one date entered
+            //date_invalid_label.setText("Enter a range of dates");
+            System.out.println("enter range");
+
+        } else if (dateStart != null && dateEnd != null && !isValidRange) {
+
+            //start date > end date if entered
+            //date_invalid_label.setText("Invalid range");
+            System.out.println("invalid rage");
+
+        } else {
+            Connection con = ConnectionConfiguration.getConnection();
+            try {
+                PreparedStatement pst = con.prepareStatement(
+                        "SELECT Type, Data_Value, DATE_TIME " +
+                                "FROM DATA_POINT WHERE Location_Name = ? ");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
     @FXML
     private void onClickResetFilter() {
+
+        dataTypeBox.getSelectionModel().clearSelection();
+
+        minDataValTextField.clear();
+        maxDataValTextField.clear();
+
+        //clears dates
+        startDate_poiDetail.getEditor().clear();
+        startDate_poiDetail.setValue(null);
+        endDate_poiDetail.getEditor().clear();
+        endDate_poiDetail.setValue(null);
 
     }
 
