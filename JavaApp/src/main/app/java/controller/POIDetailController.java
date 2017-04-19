a968d074c37b1e83a80fd73b3392b27ac7d60b96
Chaewon Min <mcw0805@gmail.com>
2017-04-18 23:33:55 -0400

    Load data type in the combo box for POI detail,  edited screen

--- JavaApp/src/main/app/java/controller/POIDetailController.java
+++ JavaApp/src/main/app/java/controller/POIDetailController.java
@@ -1,17 +1,27 @@
 package app.java.controller;
 
+import app.java.model.ConnectionConfiguration;
+import app.java.model.DataPoint;
+import app.java.model.DataType;
+import com.jfoenix.controls.JFXCheckBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 /**
@@ -21,14 +31,44 @@ public class POIDetailController implements Initializable {
 
 
     @FXML
+    private TableView poiDetailView;
+    @FXML
+    private TableColumn<DataType, String> columnDataType;
+    @FXML
+    private TableColumn<DataPoint, String> columnDataVal;
+    @FXML
+    private TableColumn<DataPoint, String> columnDateTime;
+    @FXML
+    private ComboBox dataTypeBox;
+    @FXML
+    private JFXCheckBox flagged_checkbox;
+    @FXML
+    private TextField minDataVal;
+    @FXML
+    private TextField maxDataVal;
+    @FXML
+    private DatePicker startDate_potDeatil;
+    @FXML
+    private DatePicker endDate_potDeatil;
+    @FXML
+    private Label typeRequired_label;
+    @FXML
+    private Label invalidDate_label;
+    @FXML
+    private Button applyFilter_poi_detail_btn;
+    @FXML
+    private Button resetFilter_poi_detail_btn;
+    @FXML
     private Button backBtn_poi_detail;
     @FXML
     private Text chosen_loc;
 
+    private ObservableList<String> dataTypeList = FXCollections.observableArrayList();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        loadDropDown();
     }
 
     public Scene getScene() throws IOException {
@@ -54,4 +94,39 @@ public class POIDetailController implements Initializable {
 //        //chosen_loc.setText(loc);
 //        chosen_loc.setText((new ViewPOIController()).getPOILocation());
 //    }
+
+
+    @FXML
+    private void loadDropDown() {
+
+        try {
+
+            Connection con = ConnectionConfiguration.getConnection();
+
+            //loading data type
+            PreparedStatement pstDataType = con.prepareStatement("SELECT Type FROM DATA_TYPE ORDER BY Type");
+            ResultSet rsDataType = pstDataType.executeQuery();
+
+            while (rsDataType.next()) {
+                dataTypeList.add(rsDataType.getString(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        dataTypeBox.setItems(dataTypeList);
+
+    }
+
+    @FXML
+    private void onClickApplyFilter() {
+
+    }
+
+    @FXML
+    private void onClickResetFilter() {
+
+    }
+
 }
